3bcb10924ef6a462e29e70b5c721357485249dbd
memerlul <m3m3rlul071001@gmail.com>
2025-05-12 20:30:23 +0800

    Create sidebar.php

---
+++ student_portal/sidebar.php
@@ -0,0 +1,9 @@
+<!-- sidebar.php -->
+<aside>
+  <ul>
+    <li><a href="index.php">Dashboard</a></li>
+    <li><a href="balance.php">Balance</a></li>
+    <li><a href="receipt_verification.php">Receipt Verification</a></li>
+    <li><a href="concern.php">Concerns</a></li>
+  </ul>
+</aside>
