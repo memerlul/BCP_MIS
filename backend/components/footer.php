7897d53766eba849effba55eaadf02574eda7ba8
memerlul <m3m3rlul071001@gmail.com>
2025-05-15 09:51:18 +0800

    Update footer.php

--- backend/components/footer.php
+++ backend/components/footer.php
@@ -1,10 +1,4 @@
-
-
-
-
-
-
-  <!-- ======= Footer ======= -->
+<!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
       &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
